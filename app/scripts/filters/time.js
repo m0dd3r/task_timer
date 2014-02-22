f2514430c7a7673fd59ef7f998e369bfec3e3bd5
Jason Rankin <rankin.jason@gmail.com>
2014-02-21 21:04:47 -0500

    use the moduloed seconds for minutes after calculating hours

--- app/scripts/filters/time.js
+++ app/scripts/filters/time.js
@@ -4,8 +4,8 @@ angular.module('taskTimerApp').filter('time', function () {
   return function(seconds) {
     var h = Math.floor(seconds/3600).toString();
     var s = seconds % 3600;
-    var m = Math.floor(seconds/60).toString();
-    s = seconds % 60;
+    var m = Math.floor(s/60).toString();
+    s = s % 60;
     var time = h + ':' + m + ':' + s.toString();
     return time;
   };
--- test/spec/filters/time_spec.js
+++ test/spec/filters/time_spec.js
@@ -8,6 +8,8 @@ describe('filter', function () {
   describe('time', function() {
     it('converts seconds into h:m:s format', inject(function(timeFilter) {
       expect(timeFilter(30)).toBe('0:0:30');
+      expect(timeFilter(90)).toBe('0:1:30');
+      expect(timeFilter(3690)).toBe('1:1:30');
     }));
   });
 });
